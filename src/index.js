6450de767b82321b503c051df9ea0994efab9474
haruki yamaguchi <hrk1106hasu@gmail.com>
2022-10-11 22:26:04 +0900

    finish

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {App} from './App';
-
-ReactDOM.render(<App />,document.getElementById('root'));
+import { StrictMode } from "react";
+import ReactDom from "react-dom";
+ 
+import {App} from "./App";
+ 
+ReactDom.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+  , document.getElementById("root")
+);
